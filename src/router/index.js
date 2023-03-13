569cc377a824c7654de66503c698318c3295a6a1
Era Ismaili <eraaismaili@gmail.com>
2023-03-13 21:49:20 +0100

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ import FreelancerProjects from "@/components/FreelancerProjects/FreelancerProjec
 import HomeComponent from "@/components/HomeComponent.vue"
 import SignUp from '../views/SignUp.vue'
 import LoginComp from '../views/LoginComp.vue'
-import store from '../store/index'
+//import store from '../store/index'
 
 const routes = [
   {
@@ -57,134 +57,145 @@ const routes = [
     path: "/users",
     name: "Users",
     component: ManageUsers,
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/activitylog",
     name: "ActivityLog",
     component: ActivityLog,
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/clients",
     name: "Clients",
     component: AllClients,
+    meta: { requiresAuth: true }
   },
   {
     path: "/createClientDetail",
     name: "createClientDetail",
     component: CreateClientDetail,
+    meta: { requiresAuth: true, roles: ['Client', 'Admin']}
   },
   {
     path: "/editClientDetail",
     name: "EditClientDetail",
     component: EditClientDetail,
+    meta: { requiresAuth: true, roles: ['Client', 'Admin']}
   },
   {
     path: "/clientprofile",
     name: "ClientProfile",
     component: ClientProfile,
+    meta: { requiresAuth: true, roles: ['Client']}
   },
   {
     path: "/viewclientprofile",
     name: "ViewClientProfile",
     component: ViewClientProfile,
+    meta: { requiresAuth: true }
   },
   {
     path: "/clientdetails",
     name: "ClientDetails",
     component: ListClientDetails,
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/freelancerdetails",
     name: "FreelancerDetails",
     component: ListFreelancerDetails,
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/suggestedFreelancers",
     name: "SuggestedFreelancers",
     component: SuggestedFreelancers,
+    meta: { requiresAuth: true, roles: ['Client']}
   },
   {
     path: "/createjobpost",
     name: "CreateJobPost",
     component: CreateJobPost,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Client', 'Admin']}
   },
   {
     path: "/listJobPosts",
     name: "ListJobPosts",
     component: ListJobPosts,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/myJobPosts",
     name: "MyJobPosts",
     component: MyJobPosts,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Client']}
   },
   {
     path: "/myjobapplications",
     name: "MyJobApplications",
     component: MyJobApplications,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Freelancer']}
   },
   {
     path: "/jobApplicants",
     name: "JobApplicants",
     component: JobApplicants,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Client']}
   },
   {
     path: "/JobPosts",
     name: "JobPosts",
     component: JobPosts,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true }
   },
   {
     path: "/SuggestedJobs",
     name: "SuggestedJobs",
     component: SuggestedJobs,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Freelancer']}
   },
   {
     path: "/edit",
     name: "EditJobPost",
     component: EditJobPost,
+    meta: { requiresAuth: true, roles: ['Client', 'Admin']}
   },
   {
     path: "/createcategory",
     name: "CreateCategory",
     component: CreateCategory,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/editcategory",
     name: "EditCategory",
     component: EditCategory,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/categories",
     name: "Categories",
     component: ListCategories,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/createCity",
     name: "CreateCity",
     component: CreateCity,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/editCity",
     name: "EditCity",
     component: EditCity,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/cities",
     name: "cities",
     component: ListCities,
-    //meta: { requiresAuth: true }
+    meta: { requiresAuth: true, roles: ['Admin']}
   },
   {
     path: "/signup",
@@ -200,41 +211,49 @@ const routes = [
     path: "/createFreelancerDetail",
     name: "createFreelancerDetail",
     component: CreateFreelancerDetail,
+    meta: { requiresAuth: true, roles: ['Freelancer']}
   },
   {
     path: "/editFreelancerDetail",
     name: "EditFreelancerDetail",
     component: EditFreelancerDetail,
+    meta: { requiresAuth: true, roles: ['Admin', 'Freelancer']}
   },
   {
     path: "/freelancerprofile",
     name: "FreelancerProfile",
     component: FreelancerProfile,
+    meta: { requiresAuth: true, roles: ['Freelancer']}
   },
   {
     path: "/viewfreelancerprofile",
     name: "ViewFreelancerProfile",
     component: ViewFreelancerProfile,
+    meta: { requiresAuth: true }
   },
   {
     path: "/freelancers",
     name: "Freelancers",
     component: AllFreelancers,
+    meta: { requiresAuth: true }
   },
   {
     path: "/FreelancerProjects",
     name: "FreelancerProjects",
     component: FreelancerProjects,
+    meta: { requiresAuth: true }
   },
   {
     path: "/createproject",
     name: "CreateProject",
     component: CreateProject,
+    meta: { requiresAuth: true, roles: ['Freelancer']}
   },
   {
     path: "/editProject",
     name: "EditProject",
     component: EditProject,
+    meta: { requiresAuth: true, roles: ['Freelancer']}
   },
 ];
 
@@ -245,16 +264,26 @@ const router = createRouter({
 
 
 const userId = localStorage.getItem('userId')
+const userRole = localStorage.getItem('userRole')
+
 
 router.beforeEach((to) => {
   if (to.matched.some(record => record.meta.requiresAuth)){
-  if (userId == null) {
-    router.push('/login')
-  } else if (store.state.userRole != "Admin"){
-    router.push(0)
-  }else{
-    return;
-  }
+    const roles = to.meta.roles || ''
+
+    if (userId == null) {
+      router.push('/login')
+    }
+    else if(roles == ''){
+      return;
+    }
+    else if (!roles.includes(userRole)){
+      alert("Access denied! You'll be redirected to homepage")
+      router.push('/')
+    }else{
+      return;
+    }
+
 }
 })
 
